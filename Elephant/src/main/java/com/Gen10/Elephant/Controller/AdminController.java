d2b0b89e1e6d0dd594aecbb569cc776f0e22f869
Colin Berry <colin.p.berry3@gmail.com>
2020-05-07 14:17:24 -0500

    making controller squiggles go away for Khue to test

--- Elephant/src/main/java/com/Gen10/Elephant/Controller/AdminController.java
+++ Elephant/src/main/java/com/Gen10/Elephant/Controller/AdminController.java
@@ -2,10 +2,6 @@ package com.Gen10.Elephant.Controller;
 
 import java.util.List;
 
-import com.Gen10.Elephant.dto.Location;
-import com.Gen10.Elephant.dto.User;
-import com.Gen10.Elephant.service.ServiceLayer;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.Gen10.Elephant.dto.Location;
+import com.Gen10.Elephant.dto.User;
+import com.Gen10.Elephant.service.ServiceLayer;
 
 @RestController
 @RequestMapping("api/admin")
@@ -55,10 +55,15 @@ public class AdminController {
         return ResponseEntity.ok(service.editCapacity(id, num));
     }
 
+    
+    /*
+     * 
     @PostMapping("/timeIncrement/{id}/{num}")
     public ResponseEntity<Location> editIncrement(@PathVariable int id, @PathVariable int num){
         return ResponseEntity.ok(service.editIncrement(id, num));
     }
+    
+    */
 
     @DeleteMapping("/user/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable int id) {
