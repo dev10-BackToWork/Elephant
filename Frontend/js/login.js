bfe4c93f99441050d446cb7d7f57ac43e161fe50
bettet01 <bettet01@luther.edu>
2020-05-11 14:20:07 -0500

    changed login to header params

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -7,16 +7,15 @@ function loginSubmit() {
 	var password = $("#inputPassword").val();
 	var email = $("#inputEmail").val();
 
-	var sendObject = {
-		"password": password,
-		"email": email
-	}
-
 	$.ajax({
 		type: "post",
 		url: "http://localhost:8080/api/user/login",
 		data: sendObject,
 		dataType: "json",
+		headers: {
+			"email": email,
+			"password": password
+		},
 		success: function (response) {
 			
 		}
