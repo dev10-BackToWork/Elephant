c5c83843134ba128613c79c4e958c1365602be34
Nate Wood <nwood.comp@gmail.com>
2020-05-13 11:11:18 -0500

    api map, round 2 of methods

--- Elephant/src/main/java/com/Gen10/Elephant/Controller/AdminController.java
+++ Elephant/src/main/java/com/Gen10/Elephant/Controller/AdminController.java
@@ -3,7 +3,10 @@ package com.Gen10.Elephant.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.Gen10.Elephant.dto.Arrival;
+import com.Gen10.Elephant.dto.Departure;
 import com.Gen10.Elephant.dto.Location;
+import com.Gen10.Elephant.dto.Role;
 import com.Gen10.Elephant.dto.User;
 import com.Gen10.Elephant.service.ServiceLayer;
 
@@ -46,14 +49,75 @@ public class AdminController {
         }
         return new ResponseEntity<List<User>>(new ArrayList<User>(), HttpStatus.UNAUTHORIZED);  
     }
-    
-    @GetMapping("/noAnswers")
-    public ResponseEntity<List<User>> getInactiveUsers(@RequestBody Location location, @RequestHeader("email") String email, @RequestHeader("password") String password) {
+
+    //  Round 2
+    @GetMapping("/roles")
+    public ResponseEntity<List<Role>> getRoles(@RequestHeader("email") String email, @RequestHeader("password") String password){
         User dbAdmin = service.checkAdmin(email, password);
         if(dbAdmin != null){
-            return new ResponseEntity<List<User>>(service.getInactiveUsers(location), HttpStatus.OK);
+            return new ResponseEntity<List<Role>>(service.getAllRoles(), HttpStatus.OK);
+        }
+        return new ResponseEntity<List<Role>>(new ArrayList<Role>(), HttpStatus.UNAUTHORIZED);
+    }
+
+    //  Round 2
+    @GetMapping("/occupants/{id}")
+    public ResponseEntity<List<User>> getOccupants(@PathVariable int id, @RequestHeader("email") String email, @RequestHeader("password") String password) {
+        User dbAdmin = service.checkAdmin(email, password);
+        if(dbAdmin != null){
+            return new ResponseEntity<List<User>>(service.currentUsersInOffice(id), HttpStatus.OK);
+        }
+        return new ResponseEntity<List<User>>(new ArrayList<User>(), HttpStatus.UNAUTHORIZED);
+    }
+
+    //  Round 2
+    @GetMapping("/arrivals/{id}")
+    public ResponseEntity<List<Arrival>> getArrivals(@PathVariable int id, @RequestHeader("email") String email, @RequestHeader("password") String password){
+        User dbAdmin = service.checkAdmin(email, password);
+        if(dbAdmin != null){
+            return new ResponseEntity<List<Arrival>>(service.getAllArrivalsByLocationId(id), HttpStatus.OK);
+        }
+        return new ResponseEntity<List<Arrival>>(new ArrayList<Arrival>(), HttpStatus.UNAUTHORIZED);
+    }
+    
+    //  Round 2
+    @GetMapping("/departures/{id}")
+    public ResponseEntity<List<Departure>> getDepartures(@PathVariable int id, @RequestHeader("email") String email, @RequestHeader("password") String password){
+        User dbAdmin = service.checkAdmin(email, password);
+        if(dbAdmin != null){
+            return new ResponseEntity<List<Departure>>(service.getAllDeparturesByLocationId(id), HttpStatus.OK);
+        }
+        return new ResponseEntity<List<Departure>>(new ArrayList<Departure>(), HttpStatus.UNAUTHORIZED);
+    }
+    
+    // Round 2 edited
+    @GetMapping("/noAnswers/{id}")
+    public ResponseEntity<List<User>> getInactiveUsers(@PathVariable int id, @RequestHeader("email") String email, @RequestHeader("password") String password) {
+        User dbAdmin = service.checkAdmin(email, password);
+        if(dbAdmin != null){
+            return new ResponseEntity<List<User>>(service.getInactiveUsers(id), HttpStatus.OK);
         }
         return new ResponseEntity<List<User>>(new ArrayList<User>(), HttpStatus.UNAUTHORIZED);  
+    }
+
+    // Round 2 edited
+    @GetMapping("/flagged/{id}")
+    public ResponseEntity<List<User>> getFlagged(@PathVariable int id, @RequestHeader("email") String email, @RequestHeader("password") String password) {
+        User dbAdmin = service.checkAdmin(email, password);
+        if(dbAdmin != null){
+            return new ResponseEntity<List<User>>(service.getFlaggedUsers(id), HttpStatus.OK);
+        }
+        return new ResponseEntity<List<User>>(new ArrayList<User>(), HttpStatus.UNAUTHORIZED);  
+    }
+
+    // Round 2
+    @GetMapping("/locations")
+    public ResponseEntity<List<Location>> getLocations(@RequestHeader("email") String email, @RequestHeader("password") String password){
+        User dbAdmin = service.checkAdmin(email, password);
+        if(dbAdmin != null){
+            return new ResponseEntity<List<Location>>(service.getAllLocations(), HttpStatus.OK);
+        }
+        return new ResponseEntity<List<Location>>(new ArrayList<Location>(), HttpStatus.UNAUTHORIZED);
     }
     
     @PostMapping("/newUser")
@@ -92,5 +156,16 @@ public class AdminController {
             return new ResponseEntity<Location>(service.editIncrement(id, num), HttpStatus.OK);
         }
         return new ResponseEntity<Location>(new Location(), HttpStatus.UNAUTHORIZED);
+    }
+
+    //Round 2
+    @DeleteMapping("/user/{id{")
+    public ResponseEntity<User> deleteUser(@PathVariable int id, @PathVariable int num, @RequestHeader("email") String email, @RequestHeader("password") String password) {
+        User dbAdmin = service.checkAdmin(email, password);
+        if(dbAdmin != null){
+            service.deleteUserById(id);
+            return new ResponseEntity<User>(new User(), HttpStatus.OK);
+        }
+        return new ResponseEntity<User>(new User(), HttpStatus.UNAUTHORIZED);  
     }
 }
