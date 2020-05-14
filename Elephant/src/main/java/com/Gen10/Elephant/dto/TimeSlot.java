2b6bd4ca106c8a0d87ee229aa81d79e23ba6f120
Matthew Gerszewski <dakotaseia@hotmail.com>
2020-05-14 11:31:54 -0500

    made time slots date specific, updated service layer to return only time slots for current date

--- Elephant/src/main/java/com/Gen10/Elephant/dto/TimeSlot.java
+++ Elephant/src/main/java/com/Gen10/Elephant/dto/TimeSlot.java
@@ -1,6 +1,8 @@
 package com.Gen10.Elephant.dto;
 
+import java.sql.Date;
 import java.sql.Time;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -20,6 +22,9 @@ public class TimeSlot {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int timeSlotId;
+        
+        @Column
+        private Date timeSlotDate;
 	
 	@Column
 	private Time startTime;
@@ -37,6 +42,14 @@ public class TimeSlot {
 
 	public void setTimeSlotId(int timeSlotId) {
 		this.timeSlotId = timeSlotId;
+	}
+        
+        public Date getTimeSlotDate() {
+		return this.timeSlotDate;
+	}
+
+	public void setTimeSlotDate(Date timeSlotDate) {
+		this.timeSlotDate = timeSlotDate;
 	}
 
 	public Time getStartTime() {
@@ -62,5 +75,46 @@ public class TimeSlot {
 	public void setLocation(Location location) {
 		this.location = location;
 	}
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 61 * hash + this.timeSlotId;
+        hash = 61 * hash + Objects.hashCode(this.timeSlotDate);
+        hash = 61 * hash + Objects.hashCode(this.startTime);
+        hash = 61 * hash + (this.isTaken ? 1 : 0);
+        hash = 61 * hash + Objects.hashCode(this.location);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TimeSlot other = (TimeSlot) obj;
+        if (this.timeSlotId != other.timeSlotId) {
+            return false;
+        }
+        if (this.isTaken != other.isTaken) {
+            return false;
+        }
+        if (!Objects.equals(this.timeSlotDate, other.timeSlotDate)) {
+            return false;
+        }
+        if (!Objects.equals(this.startTime, other.startTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.location, other.location)) {
+            return false;
+        }
+        return true;
+    }
 
 }
