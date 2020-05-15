872e12d49f671929a043504801cabf48d147bdca
Keely Brennan <keelybrennan4@gmail.com>
2020-05-15 17:03:15 -0500

    arrival and departure formatting

--- Elephant/src/main/resources/static/js/healthSurvey.js
+++ Elephant/src/main/resources/static/js/healthSurvey.js
@@ -3,7 +3,8 @@ $(document).ready(function () {
     $("#survey-div").hide();
     $("#screener-bye").hide();
     $("#survey-bye").hide();
-    $("#survey-authorized").hide();
+    $("#arrival-container").hide();
+    $("#departure-container").hide();
     $('#time-success').hide();
     $("#loginErr").hide();
     
@@ -28,15 +29,14 @@ var user;
                 if (response.role.roleId === 2) {
                      $("#screener-div").show();
                      $("#login").hide();
-                    alert("success - user role");
+                     alert("success - user role");
                    // window.location.replace('/healthSurvey.html');
                 } else if (response.role.roleId === 1) {
-                    alert("success - admin role");
+                     alert("success - admin role");
                      $("#login").hide();
                     //window.location.replace('/dashboard.html');
                 }
                 return false;
-
             },
             error: function (err) {
                 console.log(err);
@@ -45,13 +45,9 @@ var user;
                 clearLogin();
                 return false;
             }
-
         });
         return false;
     });
-
-console.log(user);
-
 
 function clearLogin() {
     $('#inputEmail').click(function (e) {
@@ -59,31 +55,18 @@ function clearLogin() {
         $('.form-control').val('');
     });
 };
-  
-//var user = {
-//    email,
-//    defaultPW
-//};
 
-//need to change var values to login object once on the same page
-//var password = "password";
-//var email = "user@user.com";
-//var locationId = 1;
-//var userId = 1;
-
+//================ SURVEY ==========================//
 var user;
-
-//var locationId = user.location.locationId;
-//var userId = user.userId;
-//var firstName = user.firstName;
 
 //if user is NOT coming in to the office: 
 $("#q1No").on("click", function (e) {
     e.preventDefault();
     $("#screener-div").hide();
     $("#survey-bye").hide();
-    $("#survey-authorized").hide();
-    // $("#survey-container").hide();
+    $("#arrival-container").hide();
+    $("#departure-container").hide();
+   
     console.log(user);
     var email = user.email;
     var password = user.defaultPW;
@@ -121,7 +104,8 @@ $("#q1Yes").on("click", function () {
     $("#screener-div").hide();
     $("#survey-div").show();
     $("#survey-bye").hide();
-    $("#survey-authorized").hide();
+    $("#arrival-container").hide();
+    $("#departure-container").hide();
     toggle();
 });
 
@@ -134,18 +118,15 @@ function toggle() {
     $('#q1').change(function () {
         answerOne = $(this).prop('checked');
         console.log("Q1: " + answerOne);
-        // $('#q1Equals').html(answerOne);
     });
 
     $('#q2').change(function () {
         answerTwo = $(this).prop('checked');
         console.log("Q2: " + answerTwo);
-        //$('#q2Equals').html(answerTwo);
     });
     $('#q3').change(function () {
         answerThree = $(this).prop('checked');
         console.log("Q3: " + answerThree);
-        // $('#q3Equals').html(answerThree);
     });
 }
 
@@ -153,42 +134,42 @@ function checkAuth() {
     if (answerOne === true) {
         isAuthorized = false;
         $("#survey-bye").show();
-        $("#survey-authorized").hide();
+        $("#arrival-container").hide();
+        $("#departure-container").hide();
         console.log(isAuthorized);
     } else if (answerTwo === true) {
         isAuthorized = false;
         $("#survey-bye").show();
-        $("#survey-authorized").hide();
+        $("#arrival-container").hide();
+        $("#departure-container").hide();
         console.log(isAuthorized);
     } else if (answerThree === true) {
         isAuthorized = false;
         $("#survey-bye").show();
-        $("#survey-authorized").hide();
+        $("#arrival-container").hide();
+        $("#departure-container").hide();
         console.log(isAuthorized);
     } else {
         $("#survey-bye").hide();
-        $("#survey-authorized").show();
+        $("#arrival-container").show();
+        $("#departure-container").hide();
         console.log(isAuthorized);
         isAuthorized = true;
     }
-}
-;
+};
 
 
 $("#surveySubmit").on("click", function (e) {
     e.preventDefault();
     $("#survey-container").hide();
     checkAuth();
-
     if (isAuthorized === true) {
         console.log(isAuthorized);
         loadArrivals();
     } else if (isAuthorized === false) {
         notAuthorized();
     }
-
 });
-
 
 function notAuthorized() {
         console.log(user);
@@ -216,8 +197,6 @@ function notAuthorized() {
         success: function (response, status) {
             alert('success!');
             console.log(response);
-
-
         },
         error: function (err) {
             alert('error');
@@ -235,13 +214,11 @@ function loadArrivals() {
     var password = user.defaultPW;
     var locationId = user.location.locationId;
     var userId = user.userId;
-    console.log(user.location.locationId);
-    
+   
     $.ajax({
         type: "GET",
         url: "http://localhost:8080/api/users/times/" + locationId,
-        //contentType: "application/json;charset=UTF-8",
-        //$("authorized").text("You're approved to come to the office. Next, select a time.");
+
         headers: {
             "email": email,
             "password": password
@@ -279,7 +256,9 @@ function loadArrivals() {
 
             $("#arrivalSubmit").on("click", function (e) {
                 e.preventDefault();
-                $('#survey-authorized').hide();
+                $("#arrival-container").hide();
+                //$("#departure-container").hide();
+              
                 var timeSlotId = $("#timeSelected").val();
                 console.log(timeSlotId);
                 //var timeSlotId = parseInt(timeSlotId);
@@ -299,7 +278,8 @@ function loadArrivals() {
                     success: function (response, status) {
                         console.log(response);
                         alert(response.timeSlot.startTime);
-                        $('#time-success').show();
+                        $('#arrival-success').show();
+                        $('#arrival-success').text("Your arrival time today is: " + response.timeSlot.startTime);
                         loadDepartures();
                         
                     },
@@ -317,8 +297,10 @@ function loadArrivals() {
 
 
 function loadDepartures() {
-    $('#time-success').show();
-    $('#survey-authorized').show();
+   
+    $("#arrival-container").hide();
+    $("#departure-container").show();
+    $("#arrival-success").show();
     console.log(user);
     var email = user.email;
     var password = user.defaultPW;
@@ -334,21 +316,21 @@ function loadDepartures() {
         },
         success: function (response) {
             console.log(response);
-            $("#arrival-btn-div").empty();
-            var arrivalDiv = $("#arrival-btn-div");
+            $("#departure-btn-div").empty();
+            var departureDiv = $("#departure-btn-div");
             var i;
             $.each(response, function (i, time) {
                 if (response[i].isTaken === false) {
                     var startTime = response[i].startTime;
                     var timeSlotId = response[i].timeSlotId;
                     console.log(timeSlotId + " - " + startTime);
-
-                    var arrivalBtn = "<div class='col-3'>";
-                    arrivalBtn += "<button class='btn-primary btn-lg btn-block time' id='" + timeSlotId + "'>";
-                    arrivalBtn += "<p class='item'>" + startTime + "</p>";
-                    arrivalBtn += "</button>";
-                    arrivalBtn += "</div>";
-                    arrivalDiv.append(arrivalBtn);
+                
+                    var departureBtn = "<div class='col-3'>";
+                    departureBtn += "<button class='btn-primary btn-lg btn-block time' id='" + timeSlotId + "'>";
+                    departureBtn += "<p class='item'>" + startTime + "</p>";
+                    departureBtn += "</button>";
+                    departureBtn += "</div>";
+                    departureDiv.append(departureBtn);
                 };
             });
 
@@ -357,19 +339,17 @@ function loadDepartures() {
                     console.log(this.id);
                     var timeSlotId = parseInt(this.id);
                     // var itemId = parseInt(input);
-                    console.log("Your time: " + timeSlotId);
-                    $("#timeSelected").val(timeSlotId);
+                    console.log("Your departure time: " + timeSlotId);
+                    $("#departureTimeSelected").val(timeSlotId);
                 });
             });
 
-
-            $("#arrivalSubmit").on("click", function (e) {
+            $("#departureSubmit").on("click", function (e) {
                 e.preventDefault();
-                $('#survey-authorized').hide();
-                var timeSlotId = $("#timeSelected").val();
+                //$("#arrival-container").hide();
+                $("#departure-container").hide();
+                var timeSlotId = $("#departureTimeSelected").val();
                 console.log(timeSlotId);
-                //var timeSlotId = parseInt(timeSlotId);
-                // var timeSlotId = parseInt(time);
 
                 $.ajax({
                     type: "POST",
@@ -385,19 +365,17 @@ function loadDepartures() {
                     success: function (response, status) {
                         console.log(response);
                         alert(response.timeSlot.startTime);
-                        $('#time-success').show();
-                        //return false;
+                        //$('#time-success').show();
+                        $('#departure-success').show();
+                        $('#departure-success').text("Your departure time today is: " + response.timeSlot.startTime);
                     },
                     error: function (err) {
                         console.log(err);
-                        //$("#screener-div").hide();
-                        //$("#survey-bye").show();
                         return false;
                     }
                 });
             });
         }
     });
-}
-
+    }
 });
--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -4,3 +4,50 @@ $(document).ready(function () {
 });
 
 
+//$(document).ready(function () {
+//   console.log("schedule.js!");
+//   loadTimes();
+//   
+//   });
+//   
+//    function loadTimes() {
+//     //id = 1;
+//     
+//     $.ajax({
+//        type: 'GET',
+//        url: 'http://localhost:8080/api/users/times/' + id,
+//        success: function(arrTimeArray) {
+//            console.log(arrTimeArray);
+//            $('#arrival-btn-div').empty();
+//            
+//            var arrivalDiv = $('arrival-btn-div');
+//            
+//            $.each(arrTimeArray, function (index, time) {
+//                
+//            }
+//            var itemCard = "<div class='col-3'>";
+//                itemCard += "<button class='item-card' id='" + item.id + "'>";
+//                itemCard += "<p class='item'>Id: " + item.id + "<br /></p>";
+//                itemCard += "<h5 class='item'>" + item.name + "<br /></h5>";
+//                itemCard += "<p class='item'>$" + parseFloat(item.price) + "<br /></p>";
+//                itemCard +=
+//                "<p class='item'>Quantity Left: " + item.quantity + "<br /></p>";
+//                //itemCard += "<button class='btn item-btn' id="+item.id + " value="+item.id + " 'onclick='getSelectedItem()>Select Item</button>";
+//                itemCard += "</button>";
+//               itemCard += "</div>";
+//
+//        $(function () {
+//          $(".item-card").click(function () {
+//            //console.log(this.id);
+//            var input = this.id;
+//            var itemId = parseInt(input);
+//            console.log("Your item selected: " + itemId);
+//            $("#item-input").val(itemId);
+//            console.log(input);
+//            console.log(itemId);
+//          });
+//          itemsDiv.append(itemCard);
+//        });
+//      });
+//    },
+//    });
