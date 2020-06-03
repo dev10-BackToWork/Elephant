80d25cd1327f40e5976d6d55d736f8d7e69d6fb5
Matthew Gerszewski <dakotaseia@hotmail.com>
2020-06-03 13:41:39 -0500

    Changed retrieveDatesPresent method in service layer, used custom SQL query in AttendanceRepository to filter dates/avoid errors.

--- Elephant/src/main/java/com/Gen10/Elephant/dao/AttendanceRepository.java
+++ Elephant/src/main/java/com/Gen10/Elephant/dao/AttendanceRepository.java
@@ -3,14 +3,20 @@ package com.Gen10.Elephant.dao;
 import java.time.LocalDate;
 
 import com.Gen10.Elephant.dto.Attendance;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AttendanceRepository extends JpaRepository<Attendance, Integer>{
-	
-	@Query(value = "SELECT * FROM Attendance a WHERE a.userid = ?1 and a.attendanceDate = ?2", nativeQuery = true)
-	Attendance findTodayByUser(int userId, LocalDate date);
+public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
+
+    @Query(value = "SELECT * FROM Attendance a WHERE a.userid = ?1 and a.attendanceDate = ?2", nativeQuery = true)
+    Attendance findTodayByUser(int userId, LocalDate date);
+
+    @Query(value = "SELECT * \n"
+            + "FROM attendance a\n"
+            + "WHERE a.attendanceDate >= DATE(NOW() - INTERVAL 30 DAY)", nativeQuery = true)
+    List<Attendance> findAttendanceWithinLast30Days();
 }
