4fb642b12c67295bdfbfce8585b3caa021a6a9eb
Nate Wood <nwood.comp@gmail.com>
2020-06-23 09:50:13 -0500

    working sql

--- ElephantDB.sql
+++ ElephantDB.sql
@@ -51,23 +51,23 @@ CREATE TABLE Attendance (
         REFERENCES `Location`(locationId)
 );
 
-INSERT INTO Location (cityName, maxOccupancy) VALUES 
-	("FL, Orlando (Distribution Center)", 20),
-    ("GA, Norcross", 20),
-	("KS, Lenexa", 20),
-    ("KS, Lenexa (Kansas Delivery Center)", 20),
-    ("MI, Troy", 20),
-    ("MN, Saint Paul", 20),
-    ("NC, Charlotte", 20),
-    ("NC, Charlotte (Charlotte Delivery Center)", 20),
-    ("NC, Charlotte (Charlotte Delivery Center - UV)", 20),
-    ("NY, New York", 20),
-    ("OH, Akron", 20),
-    ("Remote", 20),
-    ("TX, Austin", 20),
-    ("TX, Dallas", 20),
-    ("TX, Dallas (Plano Delivery Center)", 20),
-    ("WI, Milwaukee", 20);
+INSERT INTO Location (cityName, maxOccupancy, distributionEmail) VALUES 
+	("FL, Orlando (Distribution Center)", 20, "OutORLDC@genesis10.com"),
+    ("GA, Norcross", 20, "OutGADC@genesis10.com"),
+	("KS, Lenexa", 20, "OutKCBranch@genesis10.com"),
+    ("KS, Lenexa (Kansas Delivery Center)", 20, "OutKCDC@genesis10.com"),
+    ("MI, Troy", 20, "OUTMIDC@genesis10.com"),
+    ("MN, Saint Paul", 20, "OutMN@genesis10.com"),
+    ("NC, Charlotte", 20, "OutCLTBranch@genesis10.com"),
+    ("NC, Charlotte (Charlotte Delivery Center)", 20, "OutCLTDC@genesis10.com"),
+    ("NC, Charlotte (Charlotte Delivery Center - UV)", 20, "OutCLTDCUV@genesis10.com"),
+    ("NY, New York", 20, "OutNY@genesis10.com"),
+    ("OH, Akron", 20, "OutOH@genesis10.com"),
+    ("Remote", 20, "noreply.dev10@gmail.com"),
+    ("TX, Austin", 20, "OutAustin@genesis10.com"),
+    ("TX, Dallas", 20, "OutDALBranch@genesis10.com"),
+    ("TX, Dallas (Plano Delivery Center)", 20, "OUTMIDC@genesis10.com"),
+    ("WI, Milwaukee", 20, "OutMILW@genesis10.com");
 
 INSERT INTO `Role` (roleId, roleName) VALUES 
 	(1, "ROLE_ADMIN"),
