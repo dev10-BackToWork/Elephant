8afcbfcd0b21654cc1bb058f4bb11b900dbd3bcc
Nate Wood <nwood.comp@gmail.com>
2020-06-25 15:11:22 -0500

    sorted location

--- Elephant/src/main/java/com/Gen10/Elephant/dao/LocationRepository.java
+++ Elephant/src/main/java/com/Gen10/Elephant/dao/LocationRepository.java
@@ -9,6 +9,7 @@ import com.Gen10.Elephant.dto.Location;
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Integer>{
 
+	@Query(value = "SELECT lo.* FROM Location lo ORDER BY lo.cityName", nativeQuery = true)
 	Location getByLocationId(int id);
 
 	@Query(value = "SELECT lo.distributionEmail FROM Location lo WHERE lo.locationId = ?", nativeQuery = true)
